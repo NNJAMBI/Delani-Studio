d8bb49ae04e247425a9e0c5a5b90de392ed2be01
Nancy Njambi Karanja <nnjambi@safaricom.co.ke>
2020-06-15 15:04:45 +0300

    add id to the what we do sections and change the jquery command to toggle from slide up

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+      $("#design-text").toggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#development").click(function(){
+      $("p").slideUp("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#product").click(function(){
+      $("p").slideUp("slow");
+    });
+  });
