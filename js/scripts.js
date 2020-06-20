f7ef3a336164ef7ed8d2419ad4e4da168f9ff186
Nancy Karanja <nnjambi@safaricom.co.ke>
2020-06-20 13:30:32 +0300

    add changes made to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,6 @@ $(document).ready(function () {
     });
 });
 
-$('.main').click(function () {
-    $(this).children('.hide').slideToggle(1200);
-    $(this).children('.show').toggle(1500);
-});
-
-
 $(document).ready(function () {
     $('#overlay',).hide();
     $('#img1').animate({
